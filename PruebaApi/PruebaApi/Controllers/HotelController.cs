69abc0e433ef3dd354c85b984b0f098d2e857bd1
Reinaldo Silva Mejía <58342150+Reisilva24@users.noreply.github.com>
2021-06-29 16:20:11 +0200

    Add files via upload

---
+++ PruebaApi/PruebaApi/Controllers/HotelController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaApi.Models;
+using PruebaApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaApi.Controllers
+{
+    [Route ("api/[controller]")]
+    public class HotelController : Controller
+    {
+        private HotelSV hotelsv;
+
+
+        [HttpGet("{nights}/{destinationId}")]
+        public async Task<List<Record>> GetHotels(int nights, int destinationId)
+        {
+            if (nights <= 0)
+            {
+                //Tratar la excepción
+            }
+
+            hotelsv = new HotelSV(nights, destinationId);
+
+            var hoteles = await hotelsv.getHotels();
+
+            foreach(var iterator in hoteles)
+            {
+                for(int i =0; i<iterator.rates.Length; i++)
+                {
+                    if(iterator.rates[i].rateTypeBool){         //rateTypeBool es true cuando la reserva es por noche
+                        iterator.rates[i].value = nights * iterator.rates[i].value;
+                    }
+                }
+            }
+            return hoteles;
+        }
+    }
+}
